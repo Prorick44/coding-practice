b8e87cc7f81c4ef91a3280e4916e36cabdb69ff2
Projjal Paul <projjal2002@gmail.com>
2026-06-10 23:45:11 +0530

    dynamic programming is added in cpp

---
+++ C++/Dynamic_PRogramming/Longest_Increasing_Sunsequence.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int lengthOfLIS(vector<int> &nums)
+  {
+    vector<int> temp;
+
+    for (int num : nums)
+    {
+      auto it = lower_bound(temp.begin(), temp.end(), num);
+
+      if (it == temp.end())
+        temp.push_back(num);
+      else
+        *it = num;
+    }
+    return temp.size();
+  }
+};
